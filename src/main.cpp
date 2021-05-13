6b3488a845e6c9daaf70f54bcafc6042b3b88f54
jesus-salazar05 <j.salazarvidal@ugto.mx>
2021-05-13 02:34:33 -0500

    ImplementacionLiquidCrystal

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,20 @@
 #include <Arduino.h>
-#include <LiquidCrystal_I2C.h>
-#include <Keypad.h>
+#include <LiquidCrystal.h>
+
+
 #define btnIN A0 //Entrada analogica
-#define LED 10   //Salida digital
-/*
-char pulsacion;       // almacena la tecla pulsada
-int  contador = 1;    // cuenta el nivel del menu en el que se esta
-const byte ROWS = 4;  //Cuatro Filas
-const byte COLS = 4;  //Cuatro Columnas 
-char keys[ROWS][COLS] = {
-  {'1','2','3','A'},
-  {'4','5','6','B'},
-  {'7','8','9','C'},
-  {'*','0','#','D'}
-};
-byte rowPins[ROWS] = {9,8,7,6}; //Puntos de conexion del teclado al arduino FILAS
-byte colPins[COLS] = {5,4,3,2}; //Puntos de conexion del teclado al arduino COLUMNAS
-Keypad Teclado1 = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS ); //Configura el teclado
-LiquidCrystal_I2C lcd(0x27,16,2); // dependiendo del fabricante del I2C el codigo 0x27 cambiar a
-                                  // 0x3F , 0x20 , 0x38 , 
-*/
+#define LED 7   //Salida digital
+
+
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+int upButton = 10;
+int downButton = 9;
+int selectButton = 8;
+int menu = 1;
+
+
 enum funMode
 {
   OFF,
@@ -47,48 +42,51 @@ void funcB3(int);
 void funcB4(int);
 void funcDesva(int);
 
-/*
-void intro_0();
-void menu_1();
-void menu_2();
-void menu_3();
-void menu_4();
-void accion_1();
-void accion_2();
-void accion_3();
-void accion_4();
+void action1();
+void action2();
+void action3();
+void action4();
+
 void intermitente();
-*/
+void updateMenu();
+void execute();
+
 void setup()
 {
+  lcd.begin(16, 2);
+
+  pinMode(upButton, INPUT_PULLUP);
+  pinMode(downButton, INPUT_PULLUP);
+  pinMode(selectButton, INPUT_PULLUP);
+  updateMenu();
   pinMode(btnIN, INPUT_PULLUP);
   pinMode(LED, OUTPUT);
-  /*
-  lcd.init();
-  lcd.backlight();
-  pinMode(12, OUTPUT);
-  intro_0();             // muestra el intro de  bienvenida
-  */
+  
 }
 
 void loop()
 {
+  if (!digitalRead(downButton)) {
+    menu++;
+    updateMenu();
+    delay(100);
+  }
+
+  if (!digitalRead(upButton)) {
+    menu--;
+    updateMenu();
+    delay(100);
+    
+  }
+
+  if (!digitalRead(selectButton)) {
+    execute();
+    updateMenu();
+    delay(100);
+    
+  }	
   
-  //func1(); //Button detection
-  //func2(); //Menu de seleccion
-  funcDesva(10);
-  
-/*
-  pulsacion = Teclado1.getKey() ;  // Lee la tecla pulsada
-  if (pulsacion != 0) {      
-      lcd.clear();
-      delay(100);
-  } 
-  if(contador == 4){ menu_4();accion_4();}
-  if(contador == 3){ menu_3();accion_3();}
-  if(contador == 2){ menu_2();accion_2();}
-  if(contador == 1){ menu_1();accion_1();}
-  */
+
 }
 /////////////////////////////////////////////
 void func1()
@@ -173,6 +171,130 @@ void funcB4(int interB) {
 
 }
 
+// ####################################
+// ############# Updating #############
+// ####################################
+
+void updateMenu() {
+  switch (menu) {
+    case 0:
+      menu = 1;
+      break;
+    case 1:
+      lcd.clear();
+      lcd.print(">LED ON");
+      lcd.setCursor(0, 1);
+      lcd.print(" LED OFF");
+      break;
+    case 2:
+      lcd.clear();
+      lcd.print(" LED ON");
+      lcd.setCursor(0, 1);
+      lcd.print(">LED OFF");
+      break;
+    case 3:
+      lcd.clear();
+      lcd.print(">Desvanecido");
+      lcd.setCursor(0, 1);
+      lcd.print(" Intermitente");
+      break;
+    case 4:
+      lcd.clear();
+      lcd.print(" Desvanecido");
+      lcd.setCursor(0, 1);
+      lcd.print(">Intermitente");
+      break;
+    case 5:
+      menu = 4;
+      break;
+  }
+}
+
+
+// ####################################
+// ############ Executing #############
+// ####################################
+
+void execute() {
+  switch (menu) {
+    case 1:
+      action1();
+      break;
+    case 2:
+      action2();
+      break;
+    case 3:
+      action3();
+      break;
+     case 4:
+      action4();
+      break;
+  }
+}
+
+
+// ####################################
+// ############## Actions #############
+// ####################################
+
+void action1() {
+  lcd.clear();
+  lcd.print(">LED ON");
+  delay(150);
+  lcd.print(".");
+  delay(150);
+  lcd.print(".");
+  delay(150);
+  lcd.print(".");
+  
+  digitalWrite(LED, HIGH);
+  
+  delay(500);
+}
+
+void action2() {
+  lcd.clear();
+  lcd.print(">LED OFF");
+  delay(150);
+  lcd.print(".");
+  delay(150);
+  lcd.print(".");
+  delay(150);
+  lcd.print(".");
+  
+  digitalWrite(LED, LOW);
+  
+  delay(500);
+}
+
+void action3() {
+  lcd.clear();
+  lcd.print(">Desavecido");
+  delay(150);
+  lcd.print(".");
+  delay(150);
+  lcd.print(".");
+  delay(150);
+  lcd.print(".");
+  funcDesva(2);
+  delay(1500);
+  
+}
+
+  void action4() {
+  lcd.clear();
+  lcd.print(">Intermitente");
+  delay(150);
+  lcd.print(".");
+  delay(150);
+  lcd.print(".");
+  delay(150);
+  lcd.print(".");
+  intermitente();
+  delay(1500);
+}
+
+
 void funcDesva(int temp){//la funcion recibe un entero el cual varia la velocidad de desvanecido
   int periodo = temp;//periodo toma el valor que de temp
   //Se declaran dos tiempos los cuales se usarn para la espera o el delay
@@ -196,90 +318,17 @@ void funcDesva(int temp){//la funcion recibe un entero el cual varia la velocida
 
   }
 
-  //Prueba de cambio para repositorio remoto
+  
 }
-/*
-/////////////////////Intro_0 //////////////////////////////
-void intro_0(){ 
- lcd.clear();  // Borra el  LCD
- lcd.setCursor(3,0);         // Se posiciona en la Columna 3, Fila 0
- lcd.print("BIENVENIDO ");
- lcd.setCursor(1,1);
- lcd.print("PRUEBA DE MENU");
- delay(4000);                // espera 4 segundos mostrando el intro
- lcd.clear();
-}
-/////////////////////Menu_1  //////////////////////////////////
-void menu_1(){ 
-   lcd.setCursor(0,0);
-   lcd.print("ON    >1  ");
-   lcd.setCursor(10,0);
-   lcd.print("OFF >2");   
-   lcd.setCursor(0,1);
-   lcd.print("INTER >3  "); 
-   lcd.setCursor(10,1);
-   lcd.print("MAS >*");
-}
-/////////////////////Accion 1 //////////////////////////////
-void accion_1(){ 
-  if(pulsacion == '1') digitalWrite(12,HIGH);
-  if(pulsacion == '2') digitalWrite(12,LOW);
-  if(pulsacion == '3') intermitente();
-  if(pulsacion == '*') contador=2;
-}
-/////////////////////Menu_2  //////////////////////////////////
-void menu_2(){ 
-   lcd.setCursor(0,0);
-   lcd.print("LUZ  >1   ");
-   lcd.setCursor(8,0);
-   lcd.print("TIEMPO>2");
-   lcd.setCursor(0,1);
-   lcd.print("MENU >#          ");
-}
-/////////////////////accion_2  //////////////////////////////////
-void accion_2(){ 
-  if(pulsacion == '1') contador=3;
-  if(pulsacion == '2') contador=4;
-  if(pulsacion == '#') contador=1;
-}
-/////////////////////Menu_3  //////////////////////////////////
-void menu_3(){ 
-   lcd.setCursor(0,0);
-   lcd.print("LUZ :           ");
-   lcd.setCursor(5,0);
-   lcd.print(analogRead(A0));  
-   lcd.setCursor(0,1);
-   lcd.print("MENU>#      <--0");
-}
-/////////////////////accion_3  //////////////////////////////////
-void accion_3(){ 
-  if(pulsacion == '#') contador=1;
-  if(pulsacion == '0') contador=2;
-}
-/////////////////////Menu_4 //////////////////////////////////
-void menu_4(){ 
-   unsigned long tiempo1 = (millis()/1000);  
-   lcd.setCursor(0,0);
-   lcd.print("TIEMPO:            ");
-   lcd.setCursor(8,0);
-   lcd.print(tiempo1);  
-   lcd.setCursor(0,1);
-   lcd.print("MENU>#      <--0");
-}
-/////////////////////accion_4  //////////////////////////////////
-void accion_4(){ 
-  if(pulsacion == '#') contador=1;
-  if(pulsacion == '0') contador=2;
-}
-/////////////////////intermitente  //////////////////////////////////
-void intermitente(){ 
-  digitalWrite(12,HIGH);
+
+void intermitente(){
+  digitalWrite(LED,HIGH);
   delay(300);
-  digitalWrite(12,LOW);
+  digitalWrite(LED,LOW);
   delay(300);
-  digitalWrite(12,HIGH);
+  digitalWrite(LED,HIGH);
   delay(300);
-  digitalWrite(12,LOW);
+  digitalWrite(LED,LOW);
   delay(300);
 }
-*/
+
